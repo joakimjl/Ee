0ecf7da6b5d5083b2f7b2a1c26a4945a44d76925
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-05-27 20:18:59 +0100

    Custom Lerp Height & Rotation

---
+++ Source/EE/CombatEntity/TransformProcessor.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TransformProcessor.h"
+
+#include "EulerTransform.h"
+#include "MassCommandBuffer.h"
+#include "MassExecutionContext.h"
+#include "MassMovementFragments.h"
+#include "MassCommonFragments.h"
+#include "MassCommonTypes.h"
+#include "MassNavigationFragments.h"
+#include "Quaternion.h"
+#include "Math/UnrealMathUtility.h"
+
+
+UTransformProcessor::UTransformProcessor()
+	: EntityQuery(*this)
+{
+	ExecutionFlags = (int32)EProcessorExecutionFlags::AllNetModes;
+	ExecutionOrder.ExecuteInGroup = (UE::Mass::ProcessorGroupNames::Movement);
+	ExecutionOrder.ExecuteBefore.Add(UE::Mass::ProcessorGroupNames::Avoidance);
+}
+
+void UTransformProcessor::ConfigureQueries(const TSharedRef<FMassEntityManager>& EntityManager)
+{
+	EntityQuery.AddConstSharedRequirement<FMassMovementParameters>(EMassFragmentPresence::All);
+	EntityQuery.AddRequirement<FMassMoveTargetFragment>(EMassFragmentAccess::ReadWrite);
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadWrite);
+}
+
+void UTransformProcessor::Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context)
+{
+	const float DeltaTime = FMath::Min(0.1f, Context.GetDeltaTimeSeconds());
+
+	EntityQuery.ForEachEntityChunk(Context, [this, DeltaTime](FMassExecutionContext& Context)
+	{
+		
+		const TArrayView<FTransformFragment> TransformFragArr = Context.GetMutableFragmentView<FTransformFragment>();
+		const FMassMovementParameters MovementParams = Context.GetConstSharedFragment<FMassMovementParameters>();
+		const TArrayView<FMassMoveTargetFragment> MoveTargetArr = Context.GetMutableFragmentView<FMassMoveTargetFragment>();
+
+		for (FMassExecutionContext::FEntityIterator EntityIt = Context.CreateEntityIterator(); EntityIt; ++EntityIt)
+		{
+			FTransform& MutableTransform = TransformFragArr[EntityIt].GetMutableTransform();
+			FMassMoveTargetFragment& MoveTargetFrag = MoveTargetArr[EntityIt];
+			FVector Loc = MutableTransform.GetLocation();
+			FHitResult OutHit = FHitResult();
+			bool Res = Context.GetWorld()->LineTraceSingleByChannel(OutHit,Loc + FVector::UpVector*50.f, Loc + FVector::UpVector*-50.f, ECollisionChannel::ECC_Visibility);
+			if (Res)
+			{
+				FVector Up = OutHit.ImpactNormal;
+				FVector Forward = MoveTargetFrag.Forward;
+
+				FMatrix RotationMatrix = FRotationMatrix::MakeFromZX(Up,Forward);
+				FQuat RotationQuat = RotationMatrix.ToQuat();
+				
+				float Alpha = 0.10f;
+				
+				FQuat SmoothedRotation = FQuat::Slerp(MutableTransform.GetRotation(), RotationQuat, Alpha);
+				SmoothedRotation.Normalize();
+
+				float SmoothedHeight = FMath::Lerp(MutableTransform.GetLocation().Z,OutHit.ImpactPoint.Z,Alpha);
+				FVector SmoothedLocation = FVector(MutableTransform.GetLocation().X, MutableTransform.GetLocation().Y, SmoothedHeight);
+
+				MutableTransform.SetRotation(SmoothedRotation);
+				MutableTransform.SetLocation(SmoothedLocation);
+			}
+		}
+	});
+}
---
+++ Source/EE/CombatEntity/TransformProcessor.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/MassEntity/Public/MassProcessor.h"
+#include "TransformProcessor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EE_API UTransformProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+
+	public:
+		UTransformProcessor();
+
+	protected:
+		virtual void ConfigureQueries(const TSharedRef<FMassEntityManager>& EntityManager) override;
+		virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
+
+	private:
+		FMassEntityQuery EntityQuery;
+};
+
