40320622104c6971ea725bc259e33bdd64f9a90f
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-05-31 22:20:45 +0100

    Basic Projectile Spawn Code

---
+++ Source/EE/CombatEntity/EeSubsystem.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EeSubsystem.h"
+
+#include "CombatFragments.h"
+#include "MassCommandBuffer.h"
+#include "MassCommonFragments.h"
+#include "MassEntitySubsystem.h"
+#include "ToolContextInterfaces.h"
+
+
+void UEeSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	// Initialize dependent subsystems before calling super
+	Collection.InitializeDependency(UMassEntitySubsystem::StaticClass());
+	
+	Super::Initialize(Collection);
+	
+	EntityHandleGrid = TMap<int32, TMap<int32, TArray<FMassEntityHandle>>>();
+	EeEntityManager = &GetWorld()->GetSubsystem<UMassEntitySubsystem>()->GetMutableEntityManager();
+
+	// In here you can hook to delegates!
+	// ie: OnFireHandle = FExample::OnFireDelegate.AddUObject(this, &UMyWorldSubsystem::OnFire);
+}
+
+void UEeSubsystem::Deinitialize()
+{
+	// In here you can unhook from delegates
+	// ie: FExample::OnFireDelegate.Remove(OnFireHandle);
+	EntityHandleGrid.Empty();
+	Super::Deinitialize();
+}
+
+FIntVector2 UEeSubsystem::VectorToGrid(FVector Vector)
+{
+	FIntVector2 Out;
+	Out.X = FMath::FloorToInt(Vector.X);
+	Out.Y = FMath::FloorToInt(Vector.Y);
+	return Out;
+}
+
+FVector UEeSubsystem::MoveSelfInGrid(FMassEntityHandle Handle, FVector PreviousLocation)
+{
+	FIntVector2 GridVec = VectorToGrid(PreviousLocation);
+	if (EntityHandleGrid.Contains(GridVec.X))
+	{
+		if (EntityHandleGrid[GridVec.X].Contains(GridVec.Y))
+		{
+			if (EntityHandleGrid[GridVec.X][GridVec.Y].Contains(Handle)) EntityHandleGrid[GridVec.X][GridVec.Y].Remove(Handle);
+		}
+	}
+	FVector NewLocation = AddSelfToGrid(Handle);
+	return NewLocation;
+}
+
+FVector UEeSubsystem::AddSelfToGrid(FMassEntityHandle Handle)
+{
+	const FTransformFragment* TransformFrag = EeEntityManager->GetFragmentDataPtr<FTransformFragment>(Handle);
+	if (TransformFrag == nullptr) return FVector::ZeroVector;
+	FVector Location = TransformFrag->GetTransform().GetLocation();
+	FIntVector2 GridVec = VectorToGrid(Location);
+	if (EntityHandleGrid.Contains(GridVec.X))
+	{
+		if (EntityHandleGrid[GridVec.X].Contains(GridVec.Y))
+		{
+			if (EntityHandleGrid[GridVec.X][GridVec.Y].Contains(Handle)) EntityHandleGrid[GridVec.X][GridVec.Y].Add(Handle);
+		}
+	}
+	return Location;
+}
+
+
+bool UEeSubsystem::SpawnProjectile(FMassEntityHandle Handle)
+{
+	TArray<FMassEntityHandle> EntitiesProjectiles;
+	const UScriptStruct* FragmentTypes[] = { FTransformFragment::StaticStruct(), FProjectileParams::StaticStruct(), FProjectileFragment::StaticStruct() };
+	const FMassArchetypeHandle ArchetypeHandle = EeEntityManager->CreateArchetype(MakeArrayView(&FragmentTypes[1], 1));
+	EeEntityManager->BatchCreateEntities(ArchetypeHandle, 1, EntitiesProjectiles);
+
+	return true;
+}
---
+++ Source/EE/CombatEntity/EeSubsystem.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "EeSubsystem.generated.h"
+
+struct FMassEntityManager;
+struct FMassEntityHandle;
+/**
+ * 
+ */
+UCLASS()
+class EE_API UEeSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+
+	//Grid Functions
+	UFUNCTION()
+	FVector MoveSelfInGrid(FMassEntityHandle Handle, FVector PreviousLocation);
+	UFUNCTION()
+	FVector AddSelfToGrid(FMassEntityHandle Handle);
+	UFUNCTION(BlueprintCallable)
+	FIntVector2 VectorToGrid(FVector Vec);
+
+	
+	//Spawning Projectiles Functions
+	UFUNCTION(BlueprintCallable)
+	bool SpawnProjectile(FMassEntityHandle Handle);
+
+protected:
+	// UWorldSubsystem begin interface
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	// UWorldSubsystem end interface
+	
+private:
+	UE_MT_DECLARE_RW_ACCESS_DETECTOR(AccessDetector);
+	UPROPERTY()
+	TMap<int32, TMap<int32, TArray<FMassEntityHandle>>> EntityHandleGrid;
+	UPROPERTY()
+	FMassEntityManager* EeEntityManager;
+	
+};
+
+template<>
+struct TMassExternalSubsystemTraits<UEeSubsystem> final
+{
+	enum
+	{
+		ThreadSafeRead = true,
+		ThreadSafeWrite = false,
+		GameThreadOnly = false
+	};
+};
